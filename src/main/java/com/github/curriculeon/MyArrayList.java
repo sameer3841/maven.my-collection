3e638b984eaeb8cd612e097cd3192aa7bc3d972e
xleonhunter <xleonhunter@gmail.com>
2022-07-25 17:04:59 -0400

    added check-for-collections import

--- src/main/java/com/github/curriculeon/MyArrayList.java
+++ src/main/java/com/github/curriculeon/MyArrayList.java
@@ -1,7 +1,10 @@
 package com.github.curriculeon;
 
+import java.util.ArrayList;
+
 public class MyArrayList<_> {
     public MyArrayList() {
+        new ArrayList<>();
     }
 
     public MyArrayList(Object[] valuesToBePopulatedWith) {
